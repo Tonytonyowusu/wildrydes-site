222df69f849dd9623179ca3124477b47d783746f
Tonytonyowusu <127447834+Tonytonyowusu@users.noreply.github.com>
2025-02-14 05:54:40 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_Hz2EH3jAk', // e.g. eu-west-2_uXboG5pAb
+        userPoolClientId: ' 266j583mfndkjf15v5jo95rkju', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'London
+eu-west-2' // e.g. eu-west-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
